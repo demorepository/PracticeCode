ab011d12e769edb3dcaefd85d0e6211d7dc3aeed
Hanna-Ismahilova <hi.infoshare@gmail.com@gmail.com>
2019-10-10 22:51:28 +0200

    Constructors

---
+++ Constructor/Models/HelperInit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciseApp.Constructor.Models
+    {
+    public static class HelperInit
+        {
+        public static string ReturnUrl ( )
+            {
+            return "http://google.com";
+            }
+        }
+    }
